a0b25594fcd0f1df8ad090d0dc23a1bf8bf289cf
RajSekar1 <rajkumarviratraj07583@gmail.com>
2024-09-30 20:42:05 +0530

    hi commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,26 +2,26 @@ import React from "react";
 import AppHeader from "./components/AppHeader";
 import { Route, Routes } from "react-router-dom";
 import Product from "./components/Product";
-import Cart from "./components/cart";
+import Cart from "./components/Cart";
 
 const App = () => {
   return (
     <div className="h-fit container mx-auto bg-red-50">
-      <AppHeader/>
-        <Routes>
-          <Route path="/" element={<Product />} />
-          <Route path="/cart/:productId" element={<Cart />} />
-          <Route
-            path="*"
-            element={
-              <div className="h-full bg-blue-300">
-                <h1 className="text-5xl font-bold border-2 text-center h-screen">
-                  something went wrong
-                </h1>
-              </div>
-            }
-          />
-        </Routes>
+      <AppHeader />
+      <Routes>
+        <Route path="/" element={<Product />} />
+        <Route path="/cart/:productId" element={<Cart />} />
+        <Route
+          path="*"
+          element={
+            <div className="h-full bg-blue-300">
+              <h1 className="text-5xl font-bold border-2 text-center h-screen">
+                something went wrong
+              </h1>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 };
